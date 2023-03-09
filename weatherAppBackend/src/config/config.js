43e0d6401bd38a6462d72fa66e35df17e4b35669
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-03-09 17:28:38 -0300

    Iniciando sequelize.

---
+++ weatherAppBackend/src/config/config.js
@@ -0,0 +1,16 @@
+// src/config/config.js
+import { config } from "dotenv";
+
+const config = {
+  username: process.env.USERNAME || "root",
+  password: process.env.PASSWORD || "12345",
+  database: process.env.DB || "FavoriteCitiesDB",
+  host: process.env.HOST || "127.0.0.1",
+  dialect: "mysql",
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
