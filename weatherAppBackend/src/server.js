917a8378a33118d96e448619b384be89c2c4b478
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-03-12 01:07:10 -0300

    docker-compose criado.

--- weatherAppBackend/src/server.js
+++ weatherAppBackend/src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 
 // não remova a variável `API_PORT` ou o `listen`
-const port = process.env.API_PORT || 4000;
+const port = process.env.API_PORT || 8081;
 
 // não remova esse endpoint
 app.get("/", (_request, response) => {
